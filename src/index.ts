f0f7c4d1ae7919a31a41a0d6809ecb3128ece459
lucasellery <olucasellery@gmail.com>
2022-11-17 00:10:00 -0300

    (config) setted up the environment

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`🚀 serve  is running on http://localhost:${port}`);
+});
+
+// aula 1 => 1:00:59
