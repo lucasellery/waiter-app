0ff87a00939a221e11b1f5d907e3e8a4142c2d4d
lucasellery <olucasellery@gmail.com>
2022-12-12 08:21:13 -0300

    (feat) Add routes & add multer for files upload

---
+++ src/router.ts
@@ -0,0 +1,54 @@
+import path from 'node:path';
+import { Router } from 'express';
+import multer from 'multer';
+import { createCategory } from './app/useCases/categories/createCategory';
+import { listCategories } from './app/useCases/categories/listCategories';
+import { createProduct } from './app/useCases/products/createProduct';
+import { listProducts } from './app/useCases/products/listProducts';
+
+export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    }
+  })
+});
+
+// List categories
+router.get('/categories', listCategories);
+
+// Create category
+router.post('/categories', createCategory);
+
+// List products
+router.get('/products', listProducts);
+
+// Create products
+router.post('/products', upload.single('image'), createProduct);
+
+// Get products by category
+router.post('/categories/:categoryId/products', (req, res) => {
+  res.send('OK');
+});
+
+// List orders
+router.get('/orders', (req, res) => {
+  res.send('OK');
+});
+
+// Create order
+router.post('/orders', (req, res) => {
+  res.send('OK');
+});
+
+// Change order status
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('OK');
+});
+
+// Delete/cancel order
+router.delete('/orders/:orderId', (req, res) => {
+  res.send('OK');
+});
