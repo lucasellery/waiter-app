d4944b33d608cd5ad5eb8e81c3f8d8893878ead1
lucasellery-osf <lucas.ellery@osf.digital>
2023-02-02 23:46:21 -0300

    (fix) changes in websockets

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -7,9 +7,10 @@ export async function createOrder(req: Request, res: Response) {
     const { table, products } = req.body;
 
     const order = await Order.create({ table, products });
-    const orderDetails = await order.populate('products.product');
+    const orderDetails = order.populate('products.product');
 
     io.emit('orders@new', orderDetails);
+
     res.status(201).json(order);
   } catch (error) {
     console.log(error);
